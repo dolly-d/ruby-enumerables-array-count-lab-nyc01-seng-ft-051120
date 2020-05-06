6adebfdf2247aa1959ccbde5a2d5129afa0fb12c
Dolly Desir <dolly.desir@gmail.com>
2020-05-06 23:58:46 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -14,4 +14,13 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  total = 0
+  count = 0 
+  while count < array.length do
+    if string.length == 0 
+      total += 1 
+    end 
+    count += 1 
+  end
+  return total
 end
