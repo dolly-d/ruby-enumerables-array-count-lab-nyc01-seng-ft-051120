cc113995416667ab795b4775c4d8d09822cd6113
Dolly Desir <dolly.desir@gmail.com>
2020-05-06 23:39:11 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,14 +5,15 @@ def count_strings(array)
   row_index = 0
   
   while row_index < array.length do 
-     count = 0 
-     while count < array.length do
+    count = 0 
+    while count < array.length do
       if array[row_index][count].is_a?(String)
         total += 1 
       end 
       count += 1
-end
-return total 
+    end
+  end
+  return total 
 end
 
 def count_empty_strings(array)
