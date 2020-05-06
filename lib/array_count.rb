8be4cf42239dc9ecfaca469e3bc568924ea072cf
Dolly Desir <dolly.desir@gmail.com>
2020-05-06 23:45:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,15 +3,12 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   total = 0
   row_index = 0
-  
-  while row_index < array.length do 
-    count = 0 
-    while count < array.length do
-      if array[row_index][count].is_a?(String)
-        total += 1 
-      end 
-      count += 1
-    end
+  count = 0 
+  while count < array.length do
+    if array[row_index][count].is_a?(String)
+      total += 1 
+    end 
+    count += 1
   end
   return total 
 end
